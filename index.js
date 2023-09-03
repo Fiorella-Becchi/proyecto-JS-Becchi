3e5cf8fdc0b99539553b57ad49be82d5e9368778
Fiorella Becchi <becchifiorella@gmail.com>
2023-09-03 14:09:11 +0200

    Array y Objeto

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ if (nombreUsuario == "") {
     alert("No ingresaste el nombre de usuario");
 }
 else {
-    alert("Nombre de usuario ingresado:" + nombreUsuario);
+    alert("Nombre de usuario ingresado: " + nombreUsuario);
 }
 
 saludar();
 
-//Ciclo
+//Ciclo---- Podría hacerse una función que permita hacer los ciclos while de validación de la línea 18 y 23 para no repetir código., pero la función saludar funciona bien
 
 let precioHotel = parseFloat(prompt("Ingresa el precio del hotel:"));
 while (isNaN(precioHotel) || precioHotel < 0) {
@@ -25,13 +25,38 @@ while (isNaN(precioVuelo) || precioVuelo < 0) {
     precioVuelo = parseFloat(prompt("Ingresa el precio del vuelo:"));
 }
 
-    //Calculo
+//Calculo
 
-    const precioTotalviaje = precioHotel + precioVuelo;
-    console.log(`El precio total de tu viaje es: ${precioTotalviaje} pesos.`);
-    if (precioTotalviaje < 500) {
-        alert("Ese precio no existe");
+const precioTotalviaje = precioHotel + precioVuelo;
+console.log(`El precio total de tu viaje es: ${precioTotalviaje} pesos.`);
+if (precioTotalviaje < 500) {
+    alert("Ese precio no existe");
+}
+else if (precioTotalviaje >= 500) {
+    alert("Ahora si vas a viajar");
+
+}
+
+//array
+const destinos = ["Argentina", "Francia", "Indonesia", "Portugal"];
+const destinoElegido = prompt("Elige un destino entre: " + destinos.join(", ")); 
+
+
+//objeto
+class Viaje {
+    constructor ( tematica,tiempo, epoca){
+        this.tematica = tematica;
+        this.tiempo = tiempo;
+        this.epoca = epoca;
+        this.info = `Este viaje tiene como ${this.tematica}, durante ${this.tiempo}, en la ${this.epoca}`;
     }
-    else if (precioTotalviaje >= 500) {
-        alert("Ahora si vas a viajar");
-    }
+}
+
+let destinoArgentina = new Viaje (comidas, "14 dias", primavera);
+let destinoPortugal = new Viaje (calles,"20 dias", invierno);
+let destinoIndonesia = new Viaje (playas, "1 mes", verano);
+let destinoFrancia = new Viaje (museos, "10 dias", otoño);
+
+
+
+
